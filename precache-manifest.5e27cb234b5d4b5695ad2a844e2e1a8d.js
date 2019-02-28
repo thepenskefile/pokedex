cf33d5dbdfe8004c6c802ab7e0e3f45df67eaf93
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-28 02:47:32 +0000

    Deploy thepenskefile/pokedex to github.com/thepenskefile/pokedex.git:gh-pages

---
+++ precache-manifest.5e27cb234b5d4b5695ad2a844e2e1a8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "00637b7ec0b1ebf75096",
+    "url": "/pokedex/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "00637b7ec0b1ebf75096",
+    "url": "/pokedex/static/js/main.00637b7e.chunk.js"
+  },
+  {
+    "revision": "6c74392a225b3859b82b",
+    "url": "/pokedex/static/js/runtime~main.6c74392a.js"
+  },
+  {
+    "revision": "fe473bc3e75bef78d9e5",
+    "url": "/pokedex/static/js/2.fe473bc3.chunk.js"
+  },
+  {
+    "revision": "3a616a4e905749a47dbd",
+    "url": "/pokedex/static/js/3.3a616a4e.chunk.js"
+  },
+  {
+    "revision": "2c5f4747e36ff02b916f50fc1031e47f",
+    "url": "/pokedex/index.html"
+  }
+];
